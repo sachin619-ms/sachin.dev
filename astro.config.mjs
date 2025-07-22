96adc7918fb229d6ff5cecf6180e960d42c36d75
sachin parker <sachinparker2@gmail.com>
2025-07-22 17:53:04 +0530

    deployment process

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://sachindev.github.io",
+  base: "/sachindev",
   vite: {
     plugins: [tailwindcss()],
   },
